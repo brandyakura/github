a23e1d7fe4e7b36b507d312badc1b20929af00bb
brandyakura <126530746+brandyakura@users.noreply.github.com>
2023-03-01 22:18:27 -0300

    Add files via upload

---
+++ ProgamacionProyecto/java.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById ("boton") .addEventListener ("click", function (){
+    document.getElementById("demo") .innerHTML = "Bienvenido al Proyecto Final";
+}); 
+
+document.getElementById ("boton_default") .addEventListener ("click", function (){
+    document.getElementById("demo") .innerHTML = "Hola Mundo!";
+}); 
